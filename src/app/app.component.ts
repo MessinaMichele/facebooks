27deaeefe0bd522f00c0e9f4da4562f8462a2f42
AnonimoMC <messina.michele@iisgalvanimi.edu.it>
2023-05-31 17:19:36 +0000

    facebuk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestComponentRenderer } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '01_Angular_empty';
+}
+
+sorted_posts(): Post[]{
+  return this.posts.sort((a: PositionCallback, b: Post) => (b.like - a.like ))
+}
+submit_add_post(autore: HTMLInputElement, testo: HTMLTextAreaElement){
+ if (autore.value != ' ' && testo.value != ' ') {
+  this.post_height = 470
+  this.error_appear = false
+
+this.posts.push(new postMessage(autore.value, testo.value))
+autore.value = ' '
+testo.value = ' '
+
+  }else{
+    this.error_appear = true
+    this.post_height = 510
+  }
+
+ }
 }
